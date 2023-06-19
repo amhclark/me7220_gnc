c72042de5156f6f9397e4cbf2110c9969404aad0
Aidan Clark <57470819+amhclark@users.noreply.github.com>
2023-06-19 00:32:02 -0230

    Add files via upload
    exercise 3.1 lab 2

---
+++ simple_topic_publisher.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
+rate = rospy.Rate(2)
+vel_message = Twist()
+
+vel_message.linear.x = 0.5
+
+while not rospy.is_shutdown():
+    pub.publish(vel_message)
+
+rate.sleep()
