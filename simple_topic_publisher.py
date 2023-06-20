7277d08e10ff44bc40edd6e3f42e838b863e1506
Aidan Clark <57470819+amhclark@users.noreply.github.com>
2023-06-20 13:14:42 -0230

    Lab 2: Exercise 1.1 (Publisher)

--- simple_topic_publisher.py
+++ simple_topic_publisher.py
@@ -7,7 +7,6 @@ rospy.init_node('topic_publisher')
 pub = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 rate = rospy.Rate(2)
 vel_message = Twist()
-
 vel_message.linear.x = 0.5
 
 while not rospy.is_shutdown():
